24f652d339b0c6e1ee7d35356845bff2a7d73285
Meraki <meeeraaakiii@users.noreply.github.com>
2025-01-05 12:07:52 -0500

    02.fahrenheit-celsius-table: Modify the temperature conversion program to print a heading above the table.

--- 02.fahrenheit-celsius-table/fahrenheit-celsius-table.c
+++ 02.fahrenheit-celsius-table/fahrenheit-celsius-table.c
@@ -13,9 +13,10 @@ void main() {
 
     fahr = lower;
 
+    printf("Celsius  Fahrenheit\n");
     while (fahr <= upper) {
         celsius = (5.0/9.0) * (fahr-32.0);
-        printf("%3.0f\t%6.2f\n", fahr, celsius);
+        printf("%3.0f\t%6.1f\n", fahr, celsius);
         fahr = fahr + step;
     }
 }
