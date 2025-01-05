d5252f81d568cea13e5ed303231dcad3999cdade
Meraki <meeeraaakiii@users.noreply.github.com>
2025-01-05 11:58:52 -0500

    02.fahrenheit-celsius-table: Switch from int to float

--- 02.fahrenheit-celsius-table/fahrenheit-celsius-table.c
+++ 02.fahrenheit-celsius-table/fahrenheit-celsius-table.c
@@ -4,8 +4,8 @@
 // 0, 10, ..., 300
 
 void main() {
-    int fahr, celsius;
-    int lower, upper, step;
+    float fahr, celsius;
+    float lower, upper, step;
 
     lower = 0; // lower limit of the temperature scale
     upper = 300; // upper limit
@@ -14,8 +14,8 @@ void main() {
     fahr = lower;
 
     while (fahr <= upper) {
-        celsius = 5 * (fahr-32) / 9;
-        printf("%d\t%d\n", fahr, celsius);
+        celsius = (5.0/9.0) * (fahr-32.0);
+        printf("%3.0f\t%6.2f\n", fahr, celsius);
         fahr = fahr + step;
     }
 }
