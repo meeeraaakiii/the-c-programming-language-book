8fc7543bbea0765926ebeabc49a32b1538306daf
Meraki <meeeraaakiii@users.noreply.github.com>
2025-01-05 12:10:53 -0500

    02.fahrenheit-celsius-table: Fix heading

--- 02.fahrenheit-celsius-table/fahrenheit-celsius-table.c
+++ 02.fahrenheit-celsius-table/fahrenheit-celsius-table.c
@@ -13,7 +13,7 @@ void main() {
 
     fahr = lower;
 
-    printf("Celsius  Fahrenheit\n");
+    printf("  F          C\n");
     while (fahr <= upper) {
         celsius = (5.0/9.0) * (fahr-32.0);
         printf("%3.0f\t%6.1f\n", fahr, celsius);
