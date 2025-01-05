f42f4a2fd4d1617ec7bfca7b1f411eec9e43681c
Meraki <meeeraaakiii@users.noreply.github.com>
2025-01-05 12:48:12 -0500

    02.fahrenheit-celsius-table: Add symbolic constants, reverse the order of the table

--- 02.fahrenheit-celsius-table/fahrenheit-celsius-table-for-loop.c
+++ 02.fahrenheit-celsius-table/fahrenheit-celsius-table-for-loop.c
@@ -1,9 +1,13 @@
 #include <stdio.h>
 
+#define LOWER  0     /* lower limit of the table */
+#define UPPER  300   /* upper limit of the table */
+#define STEP   20    /* step size */
+
 void main() {
     int fahr;
 
-    for (fahr = 300; fahr >= 0; fahr -= 20) {
+    for (fahr = LOWER; fahr <= UPPER; fahr += STEP) {
         printf("%3d %6.1f\n", fahr, (5.0/9.0)*(fahr-32));
     }
 }
