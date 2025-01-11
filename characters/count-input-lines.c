5683e357724260d8f087fcdd7518c28343f6558c
Meraki <meeeraaakiii@users.noreply.github.com>
2025-01-11 17:38:54 -0500

    characters: Add count-input-lines.c

---
+++ characters/count-input-lines.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+
+// count amount of input lines
+void main() {
+    int c, nl;
+
+    nl = 0;
+    while ((c = getchar()) != EOF)
+        if (c == '\n')
+            ++nl;
+    printf("%d\n", nl);
+}
