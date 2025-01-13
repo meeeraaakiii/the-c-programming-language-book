af815e76a50290760cfb2367727c953955498418
Meraki <meeeraaakiii@users.noreply.github.com>
2025-01-12 19:54:24 -0500

    Chapter 1: Add exercise-1-19.c

---
+++ Chapter 1/exercise-1-19.c
@@ -0,0 +1,50 @@
+// Exercise 1-17. Write a program to print all input lines that are longer than 80 characters.
+#include <stdio.h>
+
+#define MAXLINE 1000 // maximum input line length
+#define THRESHOLD 80 // minimum line length to print
+
+int getLine(char line[], int maxline);
+void reverseLine(char line[], int len);
+
+// print all input lines longer than 80 characters
+int main() {
+    char line[MAXLINE]; // current input line
+    int len;            // current line length
+
+    while((len = getLine(line, MAXLINE)) > 0) {
+        reverseLine(line, len);
+        printf("%s\n", line);
+    }
+    return 0;
+}
+
+// read a line into s, return its length
+int getLine(char s[], int lim) {
+    int c, i;
+
+    for(i = 0; (c = getchar()) != EOF && c != '\n'; i++) {
+        if (i < lim - 1) {
+            s[i] = c;
+        }
+    }
+    if(c == '\n') {
+        ++i;
+    }
+    s[i] = '\0';
+    return i;
+}
+
+void reverseLine(char line[], int len) {
+    int i, j;
+    char temp;
+    // reverse the array in place
+    // start with len-2 to avoid inserting \0 at the start
+    for (i = 0, j = len - 2; i < j; i++, j--) {
+        temp = line[i];
+        line[i] = line[j];
+        line[j] = temp;
+    }
+    // don't forget null character to terminate the string
+    line[len-1] = '\0';
+}
