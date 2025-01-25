3b70e13f75b3209993f8e995d81a19cb9ecb94e5
Meraki <meeeraaakiii@users.noreply.github.com>
2025-01-25 12:55:50 -0500

    Update dir name

---
+++ chapter-1/exercise-1-22.c
@@ -0,0 +1,25 @@
+/*
+Exercise 1-22. Write a program to ``fold'' long input lines into two or more shorter lines after
+the last non-blank character that occurs before the n-th column of input. Make sure your
+program does something intelligent with very long lines, and if there are no blanks or tabs
+before the specified column.
+*/
+
+#include <stdio.h>
+
+#define MAX_LINE_LENGTH 10 // largest allowed length for a line (unless there are no spaces in it)
+
+// int fold(int maxLineLength);
+
+int fold(int maxLineLength) {
+    int i, c;
+    for (int i = 0;(c = getchar()) != EOF && c != '\n'; i++) {
+        printf("jdfsd");
+    }
+    return 0;
+}
+
+int main() {
+    fold(MAX_LINE_LENGTH);
+    return 0;
+}
